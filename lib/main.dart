ebcde9e84e610707c4c0eaefb21fbae163667342
Pc <oalaff@smail.ucas.edu.ps>
2026-06-03 22:55:07 +0300

    Initial project setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'GitHub Training Project',
+      title: 'مشروع تجريبي للتدريب الميداني',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Text("new app "),
     );
   }
 }
